0656e6bf56d2929ccc5eecaaea9fde68ba84136d
l-chang <l-chang@users.noreply.github.com>
2015-12-29 21:36:02 +0800

    Updated yyyymmdd.vhd.

--- seven_segment_indicator/yyyymmdd.vhd
+++ seven_segment_indicator/yyyymmdd.vhd
@@ -23,23 +23,47 @@ use work.ssi_display_pkg.all;
 
 entity yyyymmdd is
   generic(length: integer := 8);
+  port(
+        i_y3: in std_logic_vector(4 - 1 downto 0);
+        i_y2: in std_logic_vector(4 - 1 downto 0);
+        i_y1: in std_logic_vector(4 - 1 downto 0);
+        i_y0: in std_logic_vector(4 - 1 downto 0);
+        i_m1: in std_logic_vector(4 - 1 downto 0);
+        i_m0: in std_logic_vector(4 - 1 downto 0);
+        i_d1: in std_logic_vector(4 - 1 downto 0);
+        i_d0: in std_logic_vector(4 - 1 downto 0);
+
+        o_y3: out std_logic_vector(7 - 1 downto 0);
+        o_y2: out std_logic_vector(7 - 1 downto 0);
+        o_y1: out std_logic_vector(7 - 1 downto 0);
+        o_y0: out std_logic_vector(7 - 1 downto 0);
+        o_m1: out std_logic_vector(7 - 1 downto 0);
+        o_m0: out std_logic_vector(7 - 1 downto 0);
+        o_d1: out std_logic_vector(7 - 1 downto 0);
+        o_d0: out std_logic_vector(7 - 1 downto 0)
+      );
 end yyyymmdd;
 
 architecture rtl of yyyymmdd is
-  signal ssi_in: ssi_in_t(length - 1 downto 0);
-  signal ssi_out: ssi_out_t(length - 1 downto 0);
-
 begin
   I0: entity work.ssi_display
   generic map(length => length)
-  port map(ssi_in => ssi_in, ssi_out => ssi_out);
+  port map(
+  ssi_in(7) => i_y3,
+  ssi_in(6) => i_y2,
+  ssi_in(5) => i_y1,
+  ssi_in(4) => i_y0,
+  ssi_in(3) => i_m1,
+  ssi_in(2) => i_m0,
+  ssi_in(1) => i_d1,
+  ssi_in(0) => i_d0,
 
-  ssi_in(7) <= std_logic_vector(to_unsigned(1, 4)); -- Y
-  ssi_in(6) <= std_logic_vector(to_unsigned(9, 4)); -- Y
-  ssi_in(5) <= std_logic_vector(to_unsigned(7, 4)); -- Y
-  ssi_in(4) <= std_logic_vector(to_unsigned(0, 4)); -- Y
-  ssi_in(3) <= std_logic_vector(to_unsigned(0, 4)); -- M
-  ssi_in(2) <= std_logic_vector(to_unsigned(1, 4)); -- M
-  ssi_in(1) <= std_logic_vector(to_unsigned(0, 4)); -- D
-  ssi_in(0) <= std_logic_vector(to_unsigned(1, 4)); -- D
+  ssi_out(7) => o_y3,
+  ssi_out(6) => o_y2,
+  ssi_out(5) => o_y1,
+  ssi_out(4) => o_y0,
+  ssi_out(3) => o_m1,
+  ssi_out(2) => o_m0,
+  ssi_out(1) => o_d1,
+  ssi_out(0) => o_d0);
 end rtl;
